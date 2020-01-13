cc80a669ad9d4792c9c986dd28ff06569d4fc326
Dmytro Hryhoruk <dimamyhgrygoruk@gmail.com>
2020-01-13 14:38:00 +0200

    HW16 added few changes

--- src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
+++ src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
@@ -3,14 +3,16 @@ package mate.academy.internetshop.controller;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import mate.academy.internetshop.library.Injector;
+import org.apache.log4j.Logger;
 
 public class InjectInitializer implements ServletContextListener {
+    final static Logger logger = Logger.getLogger(InjectInitializer.class);
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         try {
             Injector.injectDependency();
         } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
+            logger.error(e);
         }
     }
 
--- src/main/java/mate/academy/internetshop/library/Injector.java
+++ src/main/java/mate/academy/internetshop/library/Injector.java
@@ -7,16 +7,18 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import org.apache.log4j.Logger;
 
 public class Injector {
     private static final String PROJECT_MAIN_PACKAGE = "mate.academy.internetshop";
+    final static Logger logger = Logger.getLogger(Injector.class);
     private static List<Class> classes = new ArrayList<>();
 
     static {
         try {
             classes.addAll(getClasses(PROJECT_MAIN_PACKAGE));
         } catch (ClassNotFoundException | IOException e) {
-            e.printStackTrace();
+            logger.error(e);
         }
     }
 
@@ -55,7 +57,7 @@ public class Injector {
 
     private static List<Class> findClasses(File directory, String packageName)
             throws ClassNotFoundException {
-        List<Class> classes = new ArrayList<Class>();
+        List<Class> classes = new ArrayList<>();
         if (!directory.exists()) {
             return classes;
         }
