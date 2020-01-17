a91879615129ff5138a663311565cf99589e4ba4
Dmytro Hryhoruk <dimamyhgrygoruk@gmail.com>
2020-01-17 10:58:36 +0200

    changed LogoutController

--- src/main/java/mate/academy/internetshop/controller/LogoutController.java
+++ src/main/java/mate/academy/internetshop/controller/LogoutController.java
@@ -12,8 +12,7 @@ public class LogoutController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        HttpSession session = req.getSession();
-        session.setAttribute("userId", null);
+        req.getSession().invalidate();
         for (Cookie cookie : req.getCookies()) {
             if (cookie.getName().equals("MATE")) {
                 cookie.setMaxAge(0);
